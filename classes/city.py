15fad1724c8c242b3df495a42eb86ac9a9aa8e7f
kateestabrook <keestabr@uvm.edu>
2025-10-06 14:41:06 -0400

    city and player

--- classes/city.py
+++ classes/city.py
@@ -3,3 +3,7 @@ City class:
 - Name
 - Sprite (not yet?)
 """
+
+class City:
+    def __init__(self, name):
+        self.name = name
--- classes/player.py
+++ classes/player.py
@@ -7,3 +7,15 @@ Player class:
 - Claimed Routes??
 - Color (ID)
 """
+# Constants
+
+INIT_TRAIN_PEICES = 45
+STARTING_POINTS = 0
+
+class Player:
+    def __init__(self, color):
+        self.color = color
+        self.train_peices = INIT_TRAIN_PEICES
+        self.points = STARTING_POINTS
+        # TODO Destinations deck
+        # TODO Train cards deck
