3f51474aeb4cce2800e82a26ca50dc1062eaeafa
kateestabrook <keestabr@uvm.edu>
2025-10-06 14:45:10 -0400

    comments cards and deck

---
+++ classes/cards.py
@@ -0,0 +1,10 @@
+"""
+- Route cards
+    - City names
+    - Points
+    - Completed/Uncompleted
+
+- Train Cards
+    - Colors of card
+    - Sprites by color (not yet)
+"""
---
+++ classes/deck.py
@@ -0,0 +1,17 @@
+"""
+Train Cards Deck (essential, hard)
+- Amount of each card remaining
+- Shuffle
+- Add card
+- Remove card
+- Name (Draw, Discard, Faceup, Player, …)
+- Deck sprites (not yet)
+
+Route Card Deck (30 routes) (essential, hard)
+- Amount of cards remaining
+- Shuffle
+- Add card
+- Remove card                                  
+- Name (Draw, Discard, Faceup, Player, …)
+- Deck sprites (not yet)
+"""
