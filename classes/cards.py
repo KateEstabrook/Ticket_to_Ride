c7507f1374590e6e89c9851fcb74851f6dadc2f6
kateestabrook <keestabr@uvm.edu>
2025-10-06 14:53:18 -0400

    cards class

--- classes/cards.py
+++ classes/cards.py
@@ -1,5 +1,5 @@
 """
-- Route cards
+- Destination cards
     - City names
     - Points
     - Completed/Uncompleted
@@ -8,3 +8,16 @@
     - Colors of card
     - Sprites by color (not yet)
 """
+
+class DestinationCard:
+    def __init__(self, city_1, city_2, points):
+        self.city_1 = city_1
+        self.city_2 = city_2
+        self.points = points
+        completed = False
+
+
+class TrainCard:
+    def __init__(self, color):
+        self.color = color
+
