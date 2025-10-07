4da588472481a742cbb74499edd7e8858998aa59
OliverBkr$123 <oebaker@uvm.edu>
2025-10-07 10:36:07 -0400

    Routes constructor updated

--- classes/route.py
+++ classes/route.py
@@ -6,10 +6,10 @@ Route class:
 - Weight (num)
 """
 class Route:
-        def __init__(self, cities, weight, color):
-            self.cities = cities
-            self.weight = weight
-            self.color = color
+        def __init__(self, route_info):
+            self.cities = (route_info[0], route_info[1])
+            self.weight = route_info[2]
+            self.color = route_info[3]
 
         def get_cities (self):
             return self.cities
