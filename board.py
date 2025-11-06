35e4ba0aa5eb71d58699aee1ab3206e37849ca2d
ekollrack <elisabethkollrack@gmail.com>
2025-11-06 10:17:11 -0500

    pop up placement

--- board.py
+++ board.py
@@ -936,7 +936,7 @@ class GameView(arcade.View):
         self.info_button.center_x = 50  # Top left position
         self.info_button.center_y = c.WINDOW_HEIGHT - 50
         self.info_button_bounds = None
-        self.showing_info_popup = False
+        self.showing_info_popup = True
 
         # Calculate info button bounds
         info_button_width = self.info_button.width
