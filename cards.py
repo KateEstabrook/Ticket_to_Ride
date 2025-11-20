5612915ab4ea6e802b583e6dc561539daa7ae5c1
ekollrack <elisabethkollrack@gmail.com>
2025-11-20 11:05:20 -0500

    pylint stuff

--- cards.py
+++ cards.py
@@ -10,7 +10,7 @@
 """
 
 class DestinationCard:
-    # Constructor
+    """Class for destination cards"""
     def __init__(self, card_info):
         self.city_1 = card_info[0]
         self.city_2 = card_info[1]
@@ -20,28 +20,35 @@ class DestinationCard:
             f"_{card_info[1].replace(' ', '')}.png"
 
     def complete(self):
+        """Complete destination card"""
         self.completed = True
 
     # Getters
     def get_points(self):
+        """Gets the points of destination card"""
         return self.points
 
     def get_city_1(self):
+        """Gets the name of the first city"""
         return self.city_1
 
     def get_city_2(self):
+        """Gets the name of the second city"""
         return self.city_2
 
     def get_completed(self):
+        """Gets the completed destination card"""
         return self.completed
 
     def get_sprite(self):
+        """Gets the sprite of destination card"""
         return self.sprite
 
     # To string
     def __str__(self):
+        """To string"""
         return f"{self.city_1} to {self.city_2}: {self.points}, Completed: {self.completed}"
-    
+
     def __eq__(self, other):
         # It's good practice to first check if 'other' is an instance of the same class
         if not isinstance(other, DestinationCard):
@@ -52,16 +59,18 @@ class DestinationCard:
 
 
 class TrainCard:
-    # Constructor
+    """Class for train cards"""
     def __init__(self, color):
         self.color = color
         self.sprite = f"images/{color}.png"
 
     # Getter
     def get_color(self):
+        """Gets the color of train card"""
         return self.color
 
     def get_sprite(self):
+        """Gets the sprite of train card"""
         return self.sprite
 
     # To string
