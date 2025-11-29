1dba0369ceaafa9e68c172cd92defa91948900af
mattb9087 <matbouqui@gmail.com>
2025-11-28 23:01:30 -0500

    pylint computer.py

--- computer.py
+++ computer.py
@@ -2,19 +2,26 @@
 Computer players
 """
 
-import player
-import deck
-import cards
+
 import globals as game_globals
-import constants as c
-import time
-import arcade
+
 
 class Computer:
+    """Computer class"""
     def __init__(self, player, game_view=None):
         self.player = player
         self.game_view = game_view
-        self.cards_needed = {"pink":0, "blue":0, "orange":0, "white":0, "green":0, "yellow":0, "black":0, "red":0, "colorless":0}
+        self.cards_needed = {
+            "pink":0,
+            "blue":0,
+            "orange":0,
+            "white":0,
+            "green":0,
+            "yellow":0,
+            "black":0,
+            "red":0,
+            "colorless":0
+        }
         self.routes_needed = []
         self.curr_dest = None
         self.turn_finished = False
@@ -23,11 +30,11 @@ class Computer:
     def get_color(self):
         """Getter for player color"""
         return self.player.get_color()
-    
+
     def get_map(self):
         """Getter for player map"""
         return self.player.get_map()
-    
+
     def get_player(self):
         """Getter for player map"""
         return self.player
@@ -37,23 +44,22 @@ class Computer:
         self.curr_dest = dest
 
     def play(self):
-        #time.sleep(0.5)
-
+        """Handles playing logic"""
         # use turn finished instead of big if elif else
         self.turn_finished = False
         self.taken_card = None
 
         # Setting current destination (at the start of game)
-        if self.curr_dest == None:
+        if self.curr_dest is None:
             self.curr_dest = self.player.get_destination_cards().get_cards()[0]
-        
+
         # Player turn decision logic
         # Update whether comp's dest card is completed and draw new card
         elif self.player.get_map().check_completed(self.curr_dest):
             self.curr_dest.complete()
             # Setting current destination if first dest is complete
             if self.player.get_destination_cards().get_uncompleted():
-                    self.curr_dest = self.player.get_destination_cards().get_uncompleted()[0]
+                self.curr_dest = self.player.get_destination_cards().get_uncompleted()[0]
             else:
                 new_card = game_globals.dest_deck.remove(-1)
                 self.player.add_card(self.player.get_destination_cards(), new_card)
@@ -62,7 +68,8 @@ class Computer:
 
         print(self.curr_dest)
 
-        # if the curr_dest of the comp is not completed, we must find the weights of paths, and if we can claim one, then claim it and end turn
+        # if the curr_dest of the comp is not completed, we must find the weights of paths,
+        # and if we can claim one, then claim it and end turn
         if not self.turn_finished:
             self.update_cards_routes_needed()
             # Check if we can claim and claim
@@ -70,20 +77,26 @@ class Computer:
                 self.turn_finished = True
 
         if not self.turn_finished and self.useful_faceup(0): # Sees a useful faceup card, draw it
-            if not self.turn_finished and self.useful_faceup(1): # Sees a useful faceup card (and it didn't draw a faceup rainbow), draw it
+            # Sees a useful faceup card (and it didn't draw a faceup rainbow), draw it
+            if not self.turn_finished and self.useful_faceup(1):
                 self.turn_finished = True
             elif not self.turn_finished: # Draw from the deck
-                self.player.add_card(self.player.get_train_cards(), game_globals.train_deck.remove(-1))
+                self.player.add_card(
+                    self.player.get_train_cards(), game_globals.train_deck.remove(-1)
+                )
                 print('Drew from deck')
                 self.turn_finished = True
 
         elif not self.turn_finished: # Draw from the train card deck if no better options
             self.player.add_card(self.player.get_train_cards(), game_globals.train_deck.remove(-1))
             print('Drew from deck')
-            if self.useful_faceup(1): # Sees a useful faceup card (and it isn't a faceup rainbow), draw it
+            # Sees a useful faceup card (and it isn't a faceup rainbow), draw it
+            if self.useful_faceup(1):
                 self.turn_finished = True
             else: # Draw from the deck
-                self.player.add_card(self.player.get_train_cards(), game_globals.train_deck.remove(-1))
+                self.player.add_card(
+                    self.player.get_train_cards(), game_globals.train_deck.remove(-1)
+                )
                 print('Drew from deck')
                 self.turn_finished = True
 
@@ -109,7 +122,7 @@ class Computer:
                 return True
 
         return False
-    
+
     def useful_faceup(self, prev):
         """"Returns boolean for if it successfully drew a useful faceup card"""
         for i in range(game_globals.faceup_deck.get_len()):
@@ -128,7 +141,7 @@ class Computer:
                 return True
             elif prev == 1 and color == 'wild':
                 continue
-            
+
             # All other colors, check if it's needed
             if self.cards_needed[color] > 0:
                 # Remove the card from face-up deck
@@ -144,12 +157,12 @@ class Computer:
                 print('took faceup card')
                 return True
         return False
-    
+
     # For creating the adjacency list for the computer player's claimed routes and the global map
     def create_adjacency_list_global(self):
         """"Creates an adjacency list of the routes the computer player has claimed"""
         adj = {}
-        map = self.player.get_map()
+        map = self.player.get_map() # pylint: disable=redefined-builtin
 
         for route in game_globals.game_map.get_paths():
             cities = route.get_cities()
@@ -162,7 +175,7 @@ class Computer:
                 adj[cities[1]] = [(cities[0], weight)]
             else:
                 adj[cities[1]].append((cities[0], weight))
-                            
+
         for route in map.get_paths(): # add self paths with weight 0
             cities = route.get_cities()
             if cities[0] not in adj:
@@ -175,29 +188,38 @@ class Computer:
                 adj[cities[1]].append((cities[0], 0))
 
         return adj
-    
+
     def update_cards_routes_needed(self):
-        """"Updates the cards needed and routes needed dictionary based on current destination card"""
-        self.cards_needed = {"pink":0, "blue":0, "orange":0, "white":0, "green":0, "yellow":0, "black":0, "red":0, "colorless":0}
+        """"Updates the cards needed and routes needed
+        dictionary based on current destination card"""
+        self.cards_needed = {
+            "pink":0,
+            "blue":0,
+            "orange":0,
+            "white":0,
+            "green":0,
+            "yellow":0,
+            "black":0,
+            "red":0,
+            "colorless":0
+        }
         self.routes_needed = []
 
         src = self.curr_dest.get_city_1()
         dest = self.curr_dest.get_city_2()
-        dist, route_lists = self.player.get_map().djikstra(src, self.create_adjacency_list_global())
+        _, route_lists = self.player.get_map().djikstra(src, self.create_adjacency_list_global())
 
-        map = self.player.get_map()
+        map = self.player.get_map() # pylint: disable=redefined-builtin
 
         # iterate through route lists at dest to find cities in path
 
         prev_city = src
         for cur_city in route_lists[dest]:
-            cur_city
-            
             routes = game_globals.game_map.get_path_by_cities(prev_city, cur_city)
 
 
             for route in routes:
-                if map.has_path(prev_city, cur_city) or route == None:
+                if map.has_path(prev_city, cur_city) or route is None:
                     prev_city = cur_city
                 else:
                     self.cards_needed[route.get_color()] += route.get_weight()
